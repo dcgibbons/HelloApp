8c33a5873193d0132bcd647c65eb00136d1d8516
Chad Gibbons <dcgibbons@gmail.com>
2012-09-22 19:10:52 -0600

    add README; HelloFramework is now a git submodule; update project to use HelloFramework via unit test

--- HelloAppTests/HelloAppTests.m
+++ HelloAppTests/HelloAppTests.m
@@ -6,27 +6,17 @@
 //  Copyright (c) 2012 Nuclear Bunny Studios, LLC. All rights reserved.
 //
 
+#import <HelloFramework/HelloFramework.h>
+
 #import "HelloAppTests.h"
 
 @implementation HelloAppTests
 
-- (void)setUp
+- (void)testBob
 {
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in HelloAppTests");
+    NSString* actual = sayHello(@"Bob");
+    NSString* expected = @"Hello there, Bob!";
+    STAssertEqualObjects(actual, expected, @"hello string did not match");
 }
 
 @end
